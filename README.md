04faea0121c90815316fc9c68a44651dd2b54a3c
Victor <victorcoolboy2009@gmail.com>
2024-08-04 01:21:24 +0530

    Fixing the README, adding config, removing boilerplate

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# victordey
-Personal Website
+# Victor Dey
+Thanks for visiting my personal portfolio website! The work is very much in progress.
--- personal-website/app/layout.tsx
+++ personal-website/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Lato } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], display: "swap", variable: "--font-in" });
+const lato = Lato({ subsets: ["latin"], display: "swap", variable: "--font-lato", weight: ["400"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.variable} ${lato.variable} font-lato bg-light dark:bg-dark`}>{children}</body>
     </html>
   );
 }
